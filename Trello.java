fa9e5afea586e99205d929424b094d788eb92787
naveen0303 <naveenchalawadi@gmail.com>
2017-10-09 18:20:28 -0500

    Created Board Class

---
+++ Trello.java
@@ -0,0 +1,25 @@
+public class Trello {
+    public static void main(String[] args) {
+        System.out.println("Enter your choice:");
+        choice:
+        {
+            System.out.println("1. Create a Board");
+            System.out.println("2. Existing Boards");
+            Board board=new Board();
+            int key = board.enterInt();
+            if(key == 1) {
+                board.CreateBoard();
+            } /* else if (key == 2) {
+            board.ExistingBoards();
+        }
+        else if (key == 3) {
+            board.DeleteBoard();
+        }*/
+
+        else{
+            System.out.println("Enter a valid option!! :");break choice;
+        }
+        }
+    }
+
+}
